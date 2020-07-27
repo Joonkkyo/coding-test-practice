2e7e8cef38a09ef997e1526806522bf8244ac9cf
Joonkyo <jkseo50@naver.com>
2020-07-27 23:59:33 +0900

    Array

---
+++ LeetCode/Toeplitz Matrix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        int M = matrix.size(); // За
+        int N = matrix[0].size(); // ї­
+        int idx;
+
+        if (M == 1 || N == 1)
+            return true;
+
+        for (int i = 0; i < M - 1; i++)
+        {
+            for (int j = 0; j < N - 1; j++)
+            {
+                if (matrix[i][j] != matrix[i + 1][j + 1])
+                    return false;
+            }
+        }
+
+        return true;
+    }
+};
