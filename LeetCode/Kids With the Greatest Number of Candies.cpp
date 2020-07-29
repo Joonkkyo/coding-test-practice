667405b0442bf83c3fa32ad8459e69f22b412901
Joonkyo <jkseo50@naver.com>
2020-07-29 23:56:09 +0900

    Array

--- LeetCode/Kids With the Greatest Number of Candies.cpp
+++ LeetCode/Kids With the Greatest Number of Candies.cpp
@@ -8,6 +8,7 @@ public:
             if (candies[i] > max)
                 max = candies[i];
         }
+        
         for (int i = 0; i < candies.size(); i++)
         {
             if (candies[i] + extraCandies < max)
