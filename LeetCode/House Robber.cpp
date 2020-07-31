110d937e19ab2356a0b7eda9b5eb8324fa67c662
Joonkyo <jkseo50@naver.com>
2020-07-31 23:33:00 +0900

    Dynamic programming

--- LeetCode/House Robber.cpp
+++ LeetCode/House Robber.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
+        int answer;
         int size = nums.size();
-        int* dp = new int[size + 1];
-        dp[0] = nums[0];
-        dp[1] = nums[1];
-        for (int i = 2; i < size; i++)
+        int dp[401];
+
+        if (size == 0)
+            return 0;
+        else if (size == 1)
+            return nums[0];
+        else if (size == 2)
+            return ((nums[0] > nums[1]) ? nums[0] : nums[1]);
+        else
         {
-            dp[i] = max(dp[i], dp[i - 2]) + nums[i];
+            dp[0] = nums[0];
+            dp[1] = nums[1];
+            dp[2] = nums[0] + nums[2];
         }
-        return max(dp[size - 1], dp[size - 2]);
+
+        for (int i = 3; i < size; i++)
+            dp[i] = max(dp[i - 2], dp[i - 3]) + nums[i];
+        answer = max(dp[size - 1], dp[size - 2]);
+        return answer;
     }
 };
