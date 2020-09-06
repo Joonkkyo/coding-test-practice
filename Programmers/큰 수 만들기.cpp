4e2f2ac3ae78fd233b8a8c7033d0628480576103
Joonkyo <jkseo50@naver.com>
2020-09-06 23:58:29 +0900

    큰 수 만들기

---
+++ Programmers/큰 수 만들기.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <math.h>
+using namespace std;
+
+string solution(string number, int k)
+{
+	string answer = "";
+	int size = number.size();
+	int max = 0;
+	int idx = 0;
+	int cnt = 0;
+
+	for (int i = 0; i < size - k; i++)
+	{
+		int temp = 0;
+		for (int j = idx; j < k + cnt + 1; j++)
+		{
+			if (max < number[j])
+			{
+				max = number[j];
+				temp = j;
+			}
+		}
+		idx = temp + 1;
+		answer += max;
+		cnt++;
+		max = 0;
+	}
+	return answer;
+}
