693ed4ae57da8ec3412b429e38e521e488aa22bf
Joonkyo <jkseo50@naver.com>
2021-05-20 10:35:52 +0900

    Stack
    easy

---
+++ LeetCode/Min Stack.py
@@ -0,0 +1,33 @@
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        
+
+    def push(self, val: int) -> None:
+        if self.stack:
+            self.stack.append((x, min(x, self.stack[-1, 1])))  # 최솟값 기록하면서 스택에 저장
+        else:
+            self.stack.append((x, x))
+            
+    def pop(self) -> None:
+        if self.stack:
+            self.stack.pop()
+
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return min(self.stack)
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
