3c0102dca93fee8ba8855cbf0999001cdad2e3ec
Joonkyo <jkseo50@naver.com>
2021-05-25 15:55:56 +0900

    Easy
    Runtime: 84 ms, faster than 41.65% of Python3 online submissions for Remove All Adjacent Duplicates In String.
    Memory Usage: 14.7 MB, less than 36.68% of Python3 online submissions for Remove All Adjacent Duplicates In String.

--- LeetCode/Stack/Remove All Adjacent Duplicates in String.py
+++ LeetCode/Stack/Remove All Adjacent Duplicates in String.py
@@ -4,18 +4,11 @@ class Solution:
         for i in range(len(s)):
             if not stack:
                 stack.append(s[i])
-                top = stack[-1]
-
             else:
-                if s[i] == top:
+                if s[i] == stack[-1]:
                     stack.pop()
-                    if stack:
-                        top = stack[-1]
-                    else:
-                        continue
                 else:
                     stack.append(s[i])
-                    top = stack[-1]
-                    
+
         return ''.join(map(str, stack))
                      
