5c734adb0c59f7d014b64819b04cdc469f10edf9
Joonkyo <jkseo50@gmail.com>
2021-06-22 18:14:27 +0900

    Create Flood Fill.py
    Runtime: 68 ms, faster than 95.70% of Python3 online submissions for Flood Fill.
    Memory Usage: 14.7 MB, less than 8.31% of Python3 online submissions for Flood Fill.

---
+++ LeetCode/DFS/Flood Fill.py
@@ -0,0 +1,19 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        row, col = len(image), len(image[0])
+        prev_color = image[sr][sc]
+        
+        def dfs(y, x):
+            if not (0 <= x < col and 0 <= y < row) or  \
+                    image[y][x] == newColor or image[y][x] != prev_color:
+                return
+
+            image[y][x] = newColor
+            dfs(y - 1, x)
+            dfs(y + 1, x)
+            dfs(y, x - 1)
+            dfs(y, x + 1)
+
+        dfs(sr, sc)
+        return image
+        
