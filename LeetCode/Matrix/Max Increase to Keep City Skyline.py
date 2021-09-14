803db0e4052624e9215bd654038bca659f36909d
Joonkyo <jkseo50@gmail.com>
2021-09-14 16:27:52 +0900

    Medium
    Runtime: 64 ms, faster than 95.79% of Python3 online submissions for Max Increase to Keep City Skyline.
    Memory Usage: 14.3 MB, less than 82.84% of Python3 online submissions for Max Increase to Keep City Skyline.

--- LeetCode/Matrix/Max Increase to Keep City Skyline.py
+++ LeetCode/Matrix/Max Increase to Keep City Skyline.py
@@ -1,23 +1,16 @@
 class Solution:
     def maxIncreaseKeepingSkyline(self, grid: List[List[int]]) -> int:
-        max_row, max_col = [], []
-        tmp_row, tmp_col = 0, 0
+        n = len(grid)
+        max_row, max_col = [0] * n, [0] * n
         answer = 0
         
-        for i in range(len(grid)):
-            tmp_row = 0
-            for j in range(len(grid[0])):
-                tmp_row = max(tmp_row, grid[i][j])
-            max_row.append(tmp_row)
-        
-        for i in range(len(grid[0])):
-            tmp_col = 0
-            for j in range(len(grid)):
-                tmp_col = max(tmp_col, grid[j][i])
-            max_col.append(tmp_col)
-            
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        for i in range(n):
+            for j in range(n):
+                max_row[i] = max(max_row[i], grid[i][j])
+                max_col[j] = max(max_col[j], grid[i][j])
+
+        for i in range(n):
+            for j in range(n):
                 criterion = min(max_row[i], max_col[j])
                 
                 if grid[i][j] < criterion:
