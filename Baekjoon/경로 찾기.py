e509de831a24f98cb68b8073ccbbe4482c7c0624
Joonkyo <jkseo50@gmail.com>
2021-10-08 02:12:59 +0900

    11403번
    Silver 1

---
+++ Baekjoon/경로 찾기.py
@@ -0,0 +1,16 @@
+N = int(input())
+graph = []
+
+for i in range(N):
+    graph.append(list(map(int, input().split())))
+    
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            if graph[i][j] == 1 or (graph[i][k] and graph[k][j] == 1):
+                graph[i][j] = 1
+
+for i in range(N):
+    for j in range(N):
+        print(graph[i][j], end=" ")
+    print()
