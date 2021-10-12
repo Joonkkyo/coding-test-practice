327892ec46defb49230ed1eaf7512dd76dc0ff1d
Joonkyo <jkseo50@gmail.com>
2021-10-13 02:22:54 +0900

    Easy
    Runtime: 36 ms, faster than 98.67% of Python3 online submissions for Maximum Depth of N-ary Tree.
    Memory Usage: 16.3 MB, less than 8.00% of Python3 online submissions for Maximum Depth of N-ary Tree.

---
+++ LeetCode/BFS/Maximum Depth of N-ary Tree.py
@@ -0,0 +1,34 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+        queue = []
+        queue.append((root, 1))
+        
+        while queue:
+            node, depth = queue.pop(0) 
+            print(node.val)
+            for child in node.children:
+                queue.append((child, depth + 1))
+     
+        return depth
+    
+#         ## DFS
+#         if not root:
+#             return 0
+        
+#         depth = 0
+
+#         for child in root.children:
+#             depth = max(depth, self.maxDepth(child))
+            
+#         return depth + 1 
+        
