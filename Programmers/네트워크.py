d54f12db2eece53a5b2467d4b7d78d303c11452b
Joonkyo <jkseo50@gmail.com>
2021-10-21 17:13:41 +0900

    DFS/BFS
    Level 3

--- Programmers/네트워크.py
+++ Programmers/네트워크.py
@@ -20,3 +20,22 @@ def solution(n, computers):
             answer += 1
             
     return answer
+
+
+### dfs
+# def solution(n, computers):
+#     answer = 0
+#     visited = [False] * n
+    
+#     def dfs(start):
+#         visited[start] = True
+#         for i in range(n):
+#             if computers[start][i] == 1 and not visited[i]:
+#                 dfs(i)
+            
+#     for i in range(n):
+#         if not visited[i]:
+#             dfs(i)
+#             answer += 1
+            
+#     return answer
