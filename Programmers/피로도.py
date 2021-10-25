ad97cc57cc1e862796bb720ba33ebe80ef936819
Joonkyo <jkseo50@gmail.com>
2021-10-25 14:51:40 +0900

    위클리 챌린지 (12주차)
    Level 2

--- Programmers/피로도.py
+++ Programmers/피로도.py
@@ -5,15 +5,15 @@ def solution(k, dungeons):
     comb = list(itertools.permutations(dungeons))
     
     for lists in comb:
-        count = 0
+        cnt = 0
         remain = k
         
         for i in range(len(dungeons)):
             if lists[i][0] <= remain:
                 remain -= lists[i][1]
-                count += 1
+                cnt += 1
             else:
                 continue
-        answer = max(count, answer)
+        answer = max(cnt, answer)
         
     return answer
