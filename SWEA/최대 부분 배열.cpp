212fb0d88ae019f156010e9ba7d022011059ad00
Joonkyo <jkseo50@gmail.com>
2023-04-28 16:15:59 +0900

    SW 문제해결 심화 - Ad Hoc Algorithms

---
+++ SWEA/최대 부분 배열.cpp
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+constexpr int MAX = 200001;
+int arr[MAX], dp[MAX];
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "r", stdin);
+
+    int T;
+    cin >> T;
+    for (int tc = 1; tc <= T; ++tc)
+    {
+        int N;
+        cin >> N;
+
+        for (register int i = 1; i <= N; i++)
+            cin >> arr[i];
+
+        int maxSum = -1e9;
+        for (int i = 1; i <= N; i++)
+        {
+            dp[i] = max(dp[i - 1], 0) + arr[i];
+            maxSum = max(maxSum, dp[i]);
+        }
+       
+        cout << '#' << tc << ' ' << maxSum << '\n';
+    }
+
+    return 0;
+}
